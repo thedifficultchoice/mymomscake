498b786da6e702c34cd2db4143faeb9026f684d7
thedifficultchoice <84717098+thedifficultchoice@users.noreply.github.com>
2021-06-21 10:08:05 +0300

    Add files via upload

---
+++ selectors.js
@@ -0,0 +1,5 @@
+const xButton = "body > div:nth-child(15) > div:nth-child(1) > div:nth-child(3) > img:nth-child(6)",
+tableWrapperSelector = "#conten > div.tablet-grid-12.grid-8.border-box",
+tableSectionSelector = "#conten > div.grid-4.tablet-hidden.border-box.right-column.pl10 > section",
+tableSelector = "#conten > div.grid-4.tablet-hidden.border-box.right-column.pl10 > section > div.content-item.tablet-pl20.tablet-pr20",
+voteButtonsSelector = "#tbody-h > tr > td:nth-child(4) > button";
